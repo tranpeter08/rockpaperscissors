f6c26b7e97fded454fa1c4bc8f64d86c6bb30c46
Peter Tran <tran.peter08@gmail.com>
2018-06-14 14:38:40 -0700

    basic function complete

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-const appSate = {
+const appState = {
     rock:{
         rock: 0,
         scissors: 1,
         paper:-1,
         img: {src: 'https://i.imgur.com/HsNPXG0.png', alt: 'A rock'}
     },
-    scissors:{ 
-        rock: -1,
-        scissors: 0,
-        paper:1,
-        img: {src: 'https://i.imgur.com/VHjYkiP.png', alt: 'A roll of toiler paper'}
-    },
     paper:{
         rock: 1,
         scissors: -1,
         paper:0,
-        img: {src: 'https://i.imgur.com/1pIL6A4.png', alt: 'A pair of scissors'}
+        img: {src: 'https://i.imgur.com/VHjYkiP.png', alt: 'A roll of toiler paper'}
+    },
+    scissors:{ 
+        rock: -1,
+        scissors: 0,
+        paper:1,
+        img: {src: 'https://i.imgur.com/1pIL6A4.png', alt: 'A roll of toiler paper'}
     }
 }
 
-function renderBoard(score){
+function renderBoard(text){
     //render result to board
-    $('.js-board').html(score)
+    $('.js-board').html(text);
 }
 
 function handleScore(user, comp){
-    let score = appSate.user.comp;
+    let score = user[comp];
     return (score === 1)? `Win`:
         (score === 0)? `Tie` : `Lose`;
 }
 
 function handleCompIMG(comp){
-    const {src, alt} = appSate[comp].img
-    $('.js-comp-img').html(`<img src="${src}" alt="${alt}">`)
+    const {src, alt} = appState[comp].img
+    $('.js-comp-img').html(`<img src="${src}" alt="${alt}">`);
 }
 
-function battleButton(user){
+
+
+function battleButton(hand){
     const comp = ['rock','paper','scissors'];
+
     $('.js-battle').on('click',()=>{
         let random = Math.floor(Math.random()*3);
         let compHand = comp[random];
-
-        handleScore(user, compHand);
-        renderBoard(handleScore);
-    })
-    ;
+        handleCompIMG(compHand);
+        renderBoard(handleScore(hand, compHand));
+    });
 }
 
 function handImg(imgSRC){
@@ -58,22 +59,21 @@ function handSelect(){
     $('.js-hand-rock').on('click',()=>{
         handImg(rock.img);
         battleButton(rock);
-    })
+    });
     //paper, img
-    $('.js-hand-rock').on('click',()=>{
+    $('.js-hand-paper').on('click',()=>{
         handImg(paper.img);
         battleButton(paper);
-    })
+    });
     //scissors, img
-    $('.js-hand-rock').on('click',()=>{
+    $('.js-hand-scissors').on('click',()=>{
         handImg(scissors.img);
         battleButton(scissors);
-    })
+    });
 }
 
 function runThis(){
     handSelect();
-    battleButton();
 }
 
 $(runThis);
